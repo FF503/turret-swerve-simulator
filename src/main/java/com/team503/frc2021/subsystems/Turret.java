482b4bd738090f20bf580b93df13621663853417
Joshua Huang <johuang50@gmail.com>
2020-10-09 09:47:04 -0400

    Complete code inspection and resolve deprecations

--- src/main/java/com/team503/frc2021/subsystems/Turret.java
+++ src/main/java/com/team503/frc2021/subsystems/Turret.java
@@ -4,22 +4,19 @@ import com.team503.frc2021.Constants;
 
 public class Turret {
     private static Turret mInstance;
-    private double demand, theta, omega, alpha, dt;
-    private long lastTime, currentTime;
+    private double demand;
+    private double theta;
+    private double omega;
+    private long lastTime;
     private ControlState mState;
 
     public Turret() {
-        demand = theta = omega = dt = alpha = currentTime = 0;
         mState = ControlState.OPEN_LOOP;
         lastTime = System.nanoTime();
     }
 
     public static Turret getInstance() {
         return mInstance == null ? mInstance = new Turret() : mInstance;
-    }
-
-    public double getDemand() {
-        return demand;
     }
 
     public void setDemand(double demand) {
@@ -35,13 +32,13 @@ public class Turret {
     }
 
     public double getTheta() {
-        currentTime = System.nanoTime();
-        dt = (currentTime - lastTime) / 1000000000.0;
+        long currentTime = System.nanoTime();
+        double dt = (currentTime - lastTime) / 1000000000.0;
         lastTime = currentTime;
 
         // Kinematic integration
-        alpha = Constants.kSimulatedLoad * (Constants.kMaxVelocity * demand - omega);
-        alpha = new Double(alpha).isNaN() ? Math.signum(alpha) * Constants.kMaxAcceleration : Math.signum(alpha) * Math.min(Constants.kMaxAcceleration, Math.abs(alpha));
+        double alpha = Constants.kSimulatedLoad * (Constants.kMaxVelocity * demand - omega);
+        alpha = Double.valueOf(alpha).isNaN() ? Math.signum(alpha) * Constants.kMaxAcceleration : Math.signum(alpha) * Math.min(Constants.kMaxAcceleration, Math.abs(alpha));
         System.out.println("RPM/S: " + alpha * 60 / 360);
 
         omega += alpha * dt;
